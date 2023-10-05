b6002f6d303384181f5b2e017e9200434bdc346f
Giorla <giorlamendozaperez@gmail.com>
2023-10-04 21:36:43 -0300

    agg el js de registro

---
+++ registro.js
@@ -0,0 +1,11 @@
+let formulario = document.querySelector('#padreform')
+let formulario1 = document.getElementById (formularioaulavirtual1)
+console.log(formulario, formulario1); 
+sessionStorage.setItem('.datos', datosdeusuario),
+sessionStorage.getItem('.datos', datosdeusuario)
+console.log();
+formulario1.addEventListener('submit')
+for (let formulario1 = 0; formulario1 < array.length; formulario1++) {
+    const element = array[formulario1];
+    
+}
